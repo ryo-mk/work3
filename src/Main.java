5b7bd945b17a64b295e8e221a7a0e0249b1c6149
maki <mkr.fb-66@cameo.plala.or.jp>
2023-03-19 18:55:35 +0900

    importを修正

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
